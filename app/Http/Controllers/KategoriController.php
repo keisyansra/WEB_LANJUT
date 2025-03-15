3434e0038fe5d3b28439ea04e56b8ab24989d6b4
keisyansra <keisya.naulia@gmail.com>
2025-03-15 16:22:06 +0700

    Praktikum 5 (Query Builder)

---
+++ app/Http/Controllers/KategoriController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class KategoriController extends Controller
+{
+    public function index() {
+        /*$data=[
+            'kategori_kode' => 'SNK',
+            'kategori_nama' => 'Snack',
+            'created_at' => now()
+        ];
+        DB::table('m_kategori')->insert($data);
+        return "Data berhasil disimpan";*/
+        //$row = DB::table('m_kategori')->where ('kategori_kode', 'SNK')->update (['kategori_nama' => 'Camilan']);
+        //return "Update berhasil. JUmlah data yang diupdate :  ".$row. " baris."; 
+        //$row = DB::table('m_kategori')->where ('kategori_kode', 'SNK')->delete();
+        //return "Data berhasil dihapus. Jumlah data yang dihapus :  ".$row. " baris.";
+        $data = DB::table('m_kategori')->get();
+        return view('kategori', ['data' => $data]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,13 @@
 <?php
 
-use App\Http\Controllers\LevelController;  
+use App\Http\Controllers\LevelController;
+use App\Http\Controllers\KategoriController;  
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DB;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/level', [LevelController::class, 'index']);
+Route::get('/kategori', [KategoriController::class, 'index']);
