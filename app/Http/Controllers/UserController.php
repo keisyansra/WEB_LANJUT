ec1b89d9435f8aecd09cf2d5b51e5f32215d0407
keisyansra <keisya.naulia@gmail.com>
2025-03-16 08:11:20 +0700

    Praktikum 1 $fillable

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,9 +10,12 @@ class UserController extends Controller
 {
     public function index() {
         $data = [
-            'nama' => 'Pelanggan Pertama',
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345')
         ];
-        UserModel::where('username', 'customer-1')->update($data); // tambah data ke tabel m_user
+        UserModel::create($data);
 
         // coba akses model UserModel 
         $user = UserModel::all(); //ambil semua data dari tabel m_user 
--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -11,4 +11,6 @@ class UserModel extends Model
 
     protected $table = 'm_user'; 
     protected $primarykey = 'user-Id';
+
+    protected $fillable = ['level_id', 'username', 'nama', 'password'];
 }
